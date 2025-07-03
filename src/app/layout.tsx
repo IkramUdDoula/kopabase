90eabeeed1089bf1641df79967d74e369f8f5d0a
IkramUdDoula <53461127+IkramUdDoula@users.noreply.github.com>
2025-07-04 02:08:20 +0600

    Update manifest and layout for PWA support; add new icons and replace logo in dashboard layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        {/* PWA and favicon links */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
       </head>
       <body className="font-body antialiased">
         {children}
