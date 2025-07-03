8553fdaba9b1a5110864f5222e702a0ab4c32199
IkramUdDoula <53461127+IkramUdDoula@users.noreply.github.com>
2025-07-04 02:53:18 +0600

    Add .netlify to .gitignore and streamline PWA configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 const withPWA = require('next-pwa');
 
-const pwaOptions = {
+const nextConfig = withPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-};
-
-const nextConfig = withPWA({
-  pwa: pwaOptions,
 });
 
 nextConfig.images = {
